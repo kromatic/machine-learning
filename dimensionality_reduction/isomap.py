8c9abf33840510c1f94b07eb4f604bbb752ca975
Leo Gladkov <leogladkov@gmail.com>
2017-01-30 00:07:59 -0600

    halfway done

--- dimensionality_reduction/isomap.py
+++ dimensionality_reduction/isomap.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python3
 
+# Leo Gladkov
+
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-def isomap(data, k):
-    """Reduce data to k dimensions using Isomap algorithm."""
-    
+def isomap(data, p):
+    """Reduce data to p dimensions using Isomap algorithm."""
+    knng = knn_graph(data[:, :-1])
+    shortest_distances = shortest_path_distances(knng)
+    gram = np.empty(shortest_distances.shape)
+    for i in range(gram.shape[0]):
+        for j in range(gram.shape[0]):
+            gram[i, j] = shortest_distances[i].dot(shortest_distances[j])
+    eigenvals, eigenvectors = np.linalg.eigh(gram)
+    eigenvals[:-p] = 0
+    sqrt_big_lambda = np.diag(np.sqrt(eigenvals))
+    res = eigenvectors.dot(sqrt_big_lambda[:, -p:])
+    return np.concatenate((res, data[:, -1:]), axis=1)
 
 def knn_graph(data, k=10):
-    """Returns matrix of edge-weights representing k-NN graph of data."""
+    """Returns matrix of edge weights representing k-NN graph of data."""
     res = np.empty((data.shape[0], )*2)
     for i in range(res.shape[0]):
         for j in range(res.shape[1]):
             res[i, j] = euclid_distance(data[i], data[j])
-        cut_off = res[i, k-1]
+        cutoff = sorted(res[i])[k-1]
         for j in range(res.shape[1]):
-            if res[i, j] > cut_off:
+            if res[i, j] > cutoff:
                 res[i, j] = np.inf
     return res
 
@@ -34,15 +46,14 @@ def euclid_distance(u, v):
     return np.linalg.norm(u-v)
 
 if __name__ == "__main__":
-    filename, k = sys.argv[1], int(sys.argv[2])
+    filename, p = sys.argv[1], int(sys.argv[2])
     data = []
     with open(filename, "r") as data_file:
         for line in data_file:
             data.append([float(x) for x in line.split()])
     data = np.array(data)
-    print(shortest_path_distances(knn_graph(data)))
-    reduced_data = isomap(data, k)
-    if k == 2:
+    reduced_data = isomap(data, p)
+    if p == 2:
         plt.title("Isomap Dimensionality Reduction")
         plt.xlabel("x")
         plt.ylabel("y")
